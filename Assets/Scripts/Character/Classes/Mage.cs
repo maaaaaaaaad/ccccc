0acfe33e7eb3ce415a50d2b72dc6df880009a0e6
maaaaaaaaad <integral5137@gmail.com>
2026-01-06 10:41:54 +0900

    .

--- Assets/Scripts/Character/Classes/Mage.cs
+++ Assets/Scripts/Character/Classes/Mage.cs
@@ -3,30 +3,37 @@ using Core;
 
 namespace Character.Classes
 {
-    public class Knight : CharacterBase
+    public class Mage : CharacterBase
     {
         protected override void Awake()
         {
-            CharacterClass = CharacterClass.Knight;
+            CharacterClass = CharacterClass.Mage;
             base.Awake();
         }
 
         protected override void InitializeStats()
         {
             Stats.SetDefaultStats(
-                maxHP: 150f,
-                maxMP: 50f,
-                attack: 25f,
-                defense: 15f,
+                maxHP: 80f,
+                maxMP: 200f,
+                attack: 30f,
+                defense: 8f,
                 moveSpeed: 2f
             );
 
-            CharacterName = "Knight";
+            CharacterName = "Mage";
         }
 
         protected override bool CanEquip(Equipment.Base.Equipment equipment)
         {
             if (equipment == null)
+            {
+                return false;
+            }
+
+            if ((equipment.EquipmentSlot, equipment.EquipmentType) is
+                (EquipmentSlot.Weapon1, not EquipmentType.Staff) or
+                (EquipmentSlot.Weapon2, not EquipmentType.Shield))
             {
                 return false;
             }
@@ -38,7 +45,7 @@ namespace Character.Classes
                 EquipmentType.Pants => true,
                 EquipmentType.Gloves => true,
                 EquipmentType.Boots => true,
-                EquipmentType.Sword => true,
+                EquipmentType.Staff => true,
                 EquipmentType.Shield => true,
                 EquipmentType.Ring => true,
                 EquipmentType.Necklace => true,
@@ -48,30 +55,9 @@ namespace Character.Classes
             };
         }
 
-        private bool IsDualWielding()
+        public float CalculateMagicPower()
         {
-            var weapon1 = GetEquippedItem(EquipmentSlot.Weapon1);
-            var weapon2 = GetEquippedItem(EquipmentSlot.Weapon2);
-
-            if (weapon1 == null || weapon2 == null)
-            {
-                return false;
-            }
-
-            return weapon1.EquipmentType == EquipmentType.Sword &&
-                   weapon2.EquipmentType == EquipmentType.Sword;
-        }
-
-        public float CalculateAttackPower()
-        {
-            var baseAttack = Stats.GetStat(StatType.Attack);
-
-            if (!IsDualWielding())
-            {
-                return baseAttack;
-            }
-
-            return baseAttack * 1.5f;
+            return Stats.GetStat(StatType.Attack);
         }
     }
 }
